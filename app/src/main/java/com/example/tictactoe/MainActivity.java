b8fa97b1af0fcc7761f668203ad648b619e0da45
FrankFr <f.freisberg@telekom.de>
2020-03-04 22:01:41 +0100

    lauffähig

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -7,12 +7,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class Game extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button f11, f12, f13, f21, f22, f23, f31, f32,f33;
     String xo = "X";
     int[][] gameStorage;
-    private Object R;
+
 
     @Override
 
@@ -22,15 +22,15 @@ public class Game extends AppCompatActivity implements View.OnClickListener {
         setContentView(R.layout.activity_main);
 
 
-    f11 = findViewById(R,id.field_1_1);
-    f12 = findViewById(R,id.field_1_2);
-    f13 = findViewById(R,id.field_1_3);
-    f21 = findViewById(R,id.field_2_1);
-    f22 = findViewById(R,id.field_2_2);
-    f23 = findViewById(R,id.field_2_3);
-    f31 = findViewById(R,id.field_3_1);
-    f32 = findViewById(R,id.field_3_2);
-    f33 = findViewById(R,id.field_3_3);
+    f11 = findViewById(R.id.field_1_1);
+    f12 = findViewById(R.id.field_1_2);
+    f13 = findViewById(R.id.field_1_3);
+    f21 = findViewById(R.id.field_2_1);
+    f22 = findViewById(R.id.field_2_2);
+    f23 = findViewById(R.id.field_2_3);
+    f31 = findViewById(R.id.field_3_1);
+    f32 = findViewById(R.id.field_3_2);
+    f33 = findViewById(R.id.field_3_3);
 
     f11.setOnClickListener(this);
     f12.setOnClickListener(this);
@@ -131,9 +131,12 @@ private boolean checkGameEnd(){
                 Toast.makeText(getApplicationContext(), "X gewinnt!",Toast.LENGTH_LONG).show();
         }
 
-        Intent intent = new Intent( this,Game.class);
+        Intent intent = new Intent( this,MainActivity.class);
         startActivity(intent);
         this.finsh();
 
+    }
+
+    private void finsh() {
     }
 }
